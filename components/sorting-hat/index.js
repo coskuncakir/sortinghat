3e3c8411ff73e951fad0a2ed4d64538bf5d1e6cd
coskun cakir <csknckr@gmail.com>
2020-09-10 13:30:42 +0100

    material ui added

--- components/sorting-hat/index.js
+++ components/sorting-hat/index.js
@@ -3,12 +3,15 @@ import useSound from "use-sound";
 import * as sound from "../../public/sounds";
 import fetcher from "../../lib/fetcher";
 import { useRouter } from "next/router";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
 
 export default function SortingHat() {
   const router = useRouter();
 
   const [name, setName] = React.useState("");
-  const [house, setHouse] = React.useState(null);
+  const [house, setHouse] = React.useState("");
 
   const [gryffindor] = useSound(sound.gryffindor);
   const [hufflepuff] = useSound(sound.hufflepuff);
@@ -29,26 +32,68 @@ export default function SortingHat() {
       slytherin();
     }
 
-    router.push("/characters");
+    const user = {
+      name,
+      house: response,
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+  const handleReset = () => {
+    setHouse("");
+    setName("");
   };
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Sorting Hat</h1>
-
-      <div>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <button type="button" onClick={handleSort}>
+    <Container maxWidth="xs" className={styles.container}>
+      <img
+        className={styles.sortingHat}
+        src="images/sorting-hat.png"
+        alt="Sorting Hat"
+      />
+      {!house && (
+        <div className={styles.textField}>
+          <TextField
+            fullWidth
+            label="Can i have your name please?"
+            onChange={(e) => setName(e.target.value)}
+            variant="outlined"
+          />
+        </div>
+      )}
+      {house && name && (
+        <h1 className={styles.welcome}>
+          Welcome {name},<br />
+          better be {house.toUpperCase()}!{" "}
+        </h1>
+      )}
+      {!house && (
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={handleSort}
+          color="secondary"
+          disabled={!name}
+        >
           Sort Me!
-        </button>
-      </div>
-    </div>
+        </Button>
+      )}
+
+      {house && (
+        <div>
+          <Button color="primary" size="small" onClick={handleReset}>
+            Nooo Sort me again!
+          </Button>
+          <Button
+            style={{ marginLeft: "16px" }}
+            onClick={() => router.push("/characters")}
+            variant="contained"
+            color="primary"
+          >
+            Continue
+          </Button>
+        </div>
+      )}
+    </Container>
   );
 }
