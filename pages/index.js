d0cb47c78e2345de7fabe7610cb85bb31850d6e3
coskun cakir <csknckr@gmail.com>
2020-12-23 12:10:56 +0000

    fix: font-size

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Sorting Hat</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
       </Head>
       <Layout>
         <SortingHat />
