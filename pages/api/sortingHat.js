6ce01c8e33d9edf8416365d1b08e4e799656a069
coskun cakir <csknckr@gmail.com>
2020-12-24 18:09:02 +0000

    fix: hard coded number to array length

--- pages/api/sortingHat.js
+++ pages/api/sortingHat.js
@@ -1,6 +1,6 @@
 export default (req, res) => {
   const houses = ["Hufflepuff", "Gryffindor", "Ravenclaw", "Slytherin"];
-  const random = Math.floor(Math.random() * 4);
+  const random = Math.floor(Math.random() * houses.length);
   res.statusCode = 200;
   res.json({ house: houses[random] });
 };
